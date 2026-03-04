e5482e2be7e2d241f90531a42881545b49cc64bf
AronJoseph96 <aronjoseph8008@gmail.com>
2026-03-05 04:49:12 +0530

    Updated project files-5feb-4:48am

--- streaming-backend/routes/movieRoutes.js
+++ streaming-backend/routes/movieRoutes.js
@@ -11,3 +11,16 @@ router.get("/", getAllMovies);           // ⭐ NEW
 router.get("/:id", getMovieById);
 
 module.exports = router;
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+router.post(
+  "/upload",
+  upload.fields([
+    { name: "poster", maxCount: 1 },
+    { name: "banner", maxCount: 1 },
+    { name: "video", maxCount: 1 }
+  ]),
+  uploadMovie
+);
